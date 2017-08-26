3e1223981f7ee42650c04e41bc09a5529468b9fa
TomatoK <1581018859@qq.com>
2017-08-26 17:22:59 +0800

    new project
    new project

---
+++ Nevermore/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Nevermore
+//
+//  Created by qiukai on 2017/8/26.
+//  Copyright © 2017年 smartisan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
