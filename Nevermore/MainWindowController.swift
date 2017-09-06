96b73c0902449723238c3e15567842369ce588d0
qiukai <qiukai@smartisan.com>
2017-09-06 15:13:35 +0800

    temp
    temp

--- Nevermore/MainWindowController.swift
+++ Nevermore/MainWindowController.swift
@@ -13,7 +13,29 @@ class MainWindowController: NSWindowController {
     
     @IBOutlet weak var button: NSButton!
     
+    var shouldShowAlert: Bool = false
+    
+    var mainWindowController: MainWindowController?
+    
     @IBAction func clickButton(_ sender: Any) {
+        
+        if shouldShowAlert {
+            let alert = NSAlert()
+            alert.messageText = "Alert message."
+            alert.informativeText = "A more detailed description of the situation."
+            alert.addButton(withTitle: "Default")
+            alert.addButton(withTitle: "Alternative")
+            alert.addButton(withTitle: "Other")
+            alert.runModal()
+            alert.beginSheetModal(for: self.window!) { (res) in
+                print("========\(res)===")
+            }
+        } else {
+            let mainWindowController = MainWindowController()
+            mainWindowController.shouldShowAlert = true
+            mainWindowController.mainWindowController = mainWindowController
+            mainWindowController.showWindow(self)
+        }
         
         print("点击了按钮")
     }
